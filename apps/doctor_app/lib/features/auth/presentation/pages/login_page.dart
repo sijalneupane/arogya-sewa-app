f6104cfb774e40fd47117ebc513aa62bc4395c66
sijalneupane <sijalneupane5@gmail.com>
2026-03-09 23:26:40 +0545

    feat: enhance DoctorLoginPage layout with padding and scrolling support

--- apps/doctor_app/lib/features/auth/presentation/pages/login_page.dart
+++ apps/doctor_app/lib/features/auth/presentation/pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:doctor_app/config/routes/routes_name.dart';
 import 'package:doctor_app/core/constants/doctor_app_strings_const.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shared_ui/utils/screen_size.dart';
 import 'package:shared_ui/widgets/auth/arogya_sewa_login_form.dart';
 
 class DoctorLoginPage extends StatelessWidget {
@@ -18,9 +19,13 @@ class DoctorLoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            ArogyaSewaLoginForm(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: context.vw(2.5),
+              vertical: context.vh(2.5),
+            ),
+            child: ArogyaSewaLoginForm(
               emailController: emailController,
               passwordController: passwordController,
               afterAuthenticationSuccess: (context) =>
@@ -30,7 +35,7 @@ class DoctorLoginPage extends StatelessWidget {
               formKey: formKey,
               // primaryColor: DoctorAppColors.primaryColor,
             ),
-          ],
+          ),
         ),
       ),
     );
--- apps/doctor_app/lib/features/splashscreen/pages/doctor_splash_screen.dart
+++ apps/doctor_app/lib/features/splashscreen/pages/doctor_splash_screen.dart
@@ -1,15 +1,23 @@
+import 'package:doctor_app/config/routes/routes_name.dart';
+import 'package:doctor_app/core/constants/doctor_app_strings_const.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:shared_ui/widgets/splash/arogya_sewa_splash_page.dart';
 
 class DoctorSplashScreen extends StatelessWidget {
   const DoctorSplashScreen({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ArogyaSewaSplashPage(appLogoImgPath: 'assets/images/doctor_app_logo.png', appTitle: 'Doctor App', afterDelayNavigate: () {
-        // Define navigation logic here
-      }),
+      body: ArogyaSewaSplashPage(
+        appLogoImgPath:appLogoWhiteBgPath,
+        appTitle: appName,
+        afterDelayNavigate: () {
+          context.pushNamed(RoutesName.loginScreen);
+        },
+      ),
     );
   }
 }
