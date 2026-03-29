abc5eb093af6bf1e8b8bacf97ea888f8c5c16414
sijalneupane <sijalneupane5@gmail.com>
2026-03-29 22:30:26 +0545

    feat: restructure shared_ui.dart to export various UI components and utilities

--- packages/shared_ui/lib/shared_ui.dart
+++ packages/shared_ui/lib/shared_ui.dart
@@ -1,5 +1,31 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// Export toast utility
+export 'widgets/app_toast.dart';
+
+// Export colors
+export 'colors/arogya_sewa_color.dart';
+
+// Export theme
+export 'theme/app_theme.dart';
+
+// Export utils
+export 'utils/hide_keyboard.dart';
+export 'utils/screen_size.dart';
+
+// Export widgets
+export 'widgets/arogya_sewa_button.dart';
+export 'widgets/arogya_sewa_interactive_viewer.dart';
+export 'widgets/arogya_sewa_loader.dart';
+export 'widgets/arogya_sewa_loading_error_builder.dart';
+export 'widgets/arogya_sewa_retry_widget.dart';
+export 'widgets/arogya_sewa_text_button.dart';
+export 'widgets/arogya_sewa_textform_field.dart';
+export 'widgets/arogya_sewa_bottom_sheet.dart';
+
+// Export search widgets
+export 'widgets/search/arogya_sewa_search_bar.dart';
+
+// Export auth widgets
+export 'widgets/auth/arogya_sewa_login_form.dart';
+
+// Export splash widgets
+export 'widgets/splash/arogya_sewa_splash_page.dart';
