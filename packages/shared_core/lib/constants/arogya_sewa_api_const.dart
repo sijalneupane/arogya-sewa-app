1f7dd0ad4409c23d6ab4e8a23d40b51a974f7aad
sijalneupane <sijalneupane5@gmail.com>
2026-03-31 23:22:47 +0545

    fix: update baseUrl in ArogyaSewaApiConst to correct API endpoint

--- packages/shared_core/lib/constants/arogya_sewa_api_const.dart
+++ packages/shared_core/lib/constants/arogya_sewa_api_const.dart
@@ -1,7 +1,9 @@
 class ArogyaSewaApiConst {
-  static const String baseUrl = "https://558f-149-88-103-54.ngrok-free.app/api/v1";
+  static const String baseUrl = "https://abd9-27-34-73-145.ngrok-free.app/api/v1";
   static const String loginUrl = "$baseUrl/auth/login";
   static const String fileUpdateUrl = "$baseUrl/file/update";
   static const String fileUploadUrl = "$baseUrl/file/upload";
   static const String loggedInUserDetails = "$baseUrl/user/details";
+  static const String doctorsUrl = "$baseUrl/doctors";
+  static const String doctorAvailabilitiesUrl = "$baseUrl/availabilities/doctor";
 }
