1a1beb78da27ae1745476b5312e54b74cdb9be1a
sijalneupane <sijalneupane5@gmail.com>
2026-04-02 08:53:48 +0545

    feat: khalit verification params

---
+++ apps/patient_app/lib/features/payments/domain/usecases/params/verify_khalti_payment_params.dart
@@ -0,0 +1,10 @@
+/// Parameters for verifying Khalti payment
+class VerifyKhaltiPaymentParams {
+  final String appointmentId;
+  final String pidx;
+
+  VerifyKhaltiPaymentParams({
+    required this.appointmentId,
+    required this.pidx,
+  });
+}
