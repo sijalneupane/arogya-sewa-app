18207bd855155e344559923cb265acf56f1a80c4
sijalneupane <sijalneupane5@gmail.com>
2026-04-02 21:05:32 +0545

    feat: add rememberMe field to LoginEntity for user preference handling

--- packages/shared_feature/lib/auth/domain/entity/login_entity.dart
+++ packages/shared_feature/lib/auth/domain/entity/login_entity.dart
@@ -2,10 +2,12 @@ class LoginEntity {
   final String email;
   final String password;
   final String fcmToken;
+  final bool rememberMe;
 
   const LoginEntity({
     required this.email,
     required this.password,
     required this.fcmToken,
+    this.rememberMe = false,
   });
 }
